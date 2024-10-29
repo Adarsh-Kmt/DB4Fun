d22a36c8972f53964106252427db38449511d1ac
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-10-29 21:39:07 +0530

    Built a simple CLI program to test frontend and B Tree.

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/Adarsh-Kmt/DB4Fun/backend/btree"
 	"github.com/Adarsh-Kmt/DB4Fun/frontend"
 )
 
@@ -13,24 +17,123 @@ var (
 
 func main() {
 
-	str := "SELECT first_name, last_name FROM user_table WHERE first_name = 'adarsh' AND last_name = 'kamath';"
-	tokenArray, err := frontend.Tokenizer(str)
+	scanner := bufio.NewReader(os.Stdin)
+	fmt.Println("DB4Fun")
+	fmt.Println("1) enter 'exit' to exit terminal")
+	fmt.Println("2) enter 'test-btree' to test working of b tree with maximum 3 items per node.")
+	fmt.Println("3) enter 'test-frontend' to test SQL tokenizer and parser. ")
+	for {
+		logger.Print("enter input: ")
+		input, err := scanner.ReadString('\n')
+		input = strings.TrimRight(input, "\r\n")
+		logger.Println("string received : " + input)
+		if err != nil {
+			return
+		}
+		if input == "exit" {
+			logger.Println("exiting...")
+			return
+		}
 
-	if err != nil {
-		logger.Printf("%s", err.Error())
-		return
+		if input == "test-btree" {
+
+			logger.Println("testing b-tree...")
+			bTree := btree.BTreeInit(3)
+			ok := true
+			for ok {
+				logger.Print("enter key : ")
+				key, err := scanner.ReadString('\n')
+				key = strings.TrimRight(key, "\r\n")
+				if err != nil {
+					return
+				}
+				logger.Print("enter value : ")
+				value, err := scanner.ReadString('\n')
+				value = strings.TrimRight(value, "\r\n")
+				if err != nil {
+					return
+				}
+
+				bTree.InsertItem(key, value)
+				logger.Print("continue? (Y/N)")
+
+				YNResponse, err := scanner.ReadString('\n')
+				YNResponse = strings.TrimRight(YNResponse, "\r\n")
+				if err != nil {
+					return
+				}
+
+				if strings.Compare(YNResponse, "N") == 0 {
+					ok = false
+				}
+			}
+		}
+		if input == "test-frontend" {
+
+			logger.Println("enter SQL statement : ")
+			str, err := scanner.ReadString('\n')
+			str = strings.TrimRight(str, "\r\n")
+			if err != nil {
+				return
+			}
+			tokenArray, err := frontend.Tokenizer(str)
+
+			if err != nil {
+				logger.Printf("%s", err.Error())
+
+			}
+
+			DisplayTokenizerOutput(tokenArray)
+
+			selectStatement, err := frontend.ParseSelectStatment(tokenArray)
+
+			if err != nil {
+				logger.Println(err.Error())
+
+			}
+
+			DisplayParserOutput(selectStatement)
+
+		}
+
 	}
 
-	DisplayTokenizerOutput(tokenArray)
+	// if true {
 
-	selectStatement, err := frontend.ParseSelectStatment(tokenArray)
+	// 	bTree := btree.BTreeInit(4)
+	// 	bTree.InsertItem("4", "a")
+	// 	bTree.InsertItem("5", "b")
+	// 	bTree.InsertItem("6", "c")
+	// 	bTree.InsertItem("9", "d")
+	// 	bTree.InsertItem("10", "e")
+	// 	bTree.InsertItem("3", "f")
+	// 	bTree.InsertItem("2", "g")
+	// 	bTree.InsertItem("8", "h")
+	// 	bTree.InsertItem("7", "i")
+	// 	bTree.InsertItem("11", "i")
+	// 	bTree.InsertItem("1", "j")
+	// 	bTree.InsertItem("0", "k")
+	// 	//backend.BTreeTraversal(bTree.Root)
+	// 	return
+	// }
+	// str := "SELECT first_name, last_name FROM user_table WHERE first_name = 'adarsh' AND last_name = 'kamath';"
+	// tokenArray, err := frontend.Tokenizer(str)
 
-	if err != nil {
-		logger.Println(err.Error())
-		return
-	}
+	// if err != nil {
+	// 	logger.Printf("%s", err.Error())
+	// 	return
+	// }
 
-	DisplayParserOutput(selectStatement)
+	// DisplayTokenizerOutput(tokenArray)
+
+	// selectStatement, err := frontend.ParseSelectStatment(tokenArray)
+
+	// if err != nil {
+	// 	logger.Println(err.Error())
+	// 	return
+	// }
+
+	// DisplayParserOutput(selectStatement)
 
 }
 
